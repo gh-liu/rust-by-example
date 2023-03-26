8223e91b285d5ea853362f153335656ab519a656
gh-liu <liugh.cs@gmail.com>
2023-03-26 21:35:33 +0800

    feat: expression

---
+++ expression/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // statement
+
+    // expression
+    // blocks are expressions, so the can be used as values in assignments;
+    // the last expression in block wiil be assigned.
+    let z = { 2 * 2 };
+    println!("{}", z);
+}
