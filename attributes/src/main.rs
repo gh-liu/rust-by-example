7efbb5ea92ae188b2fdd1ab64ef63e4a2aa2552c
gh-liu <liugh.cs@gmail.com>
2023-03-28 17:29:10 +0800

    feat: attribute

---
+++ attributes/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // An attribute is metadata applied to some module, crete or item.
+    // can be used to/for:
+    // 1. conditional compliation of code
+    // 2. set create name, version, and type(binary or library)
+    // 3. disable lints
+    // 4. enable compiler features(macros, glob import, etc)
+    // 5. link to a foregin library
+    // 6. mark functions as unit test
+    // 7. mark functions that will be part of benchmark
+    // 8. attribute like macros
+}
