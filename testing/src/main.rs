45a77a8cf65c30f2c41e05363f9dd01acb5cd844
gh-liu <liugh.cs@gmail.com>
2023-04-03 20:30:24 +0800

    feat: testing

---
+++ testing/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // 1. unit testing
+    // Most unit tests go into a tests mod with the #[cfg(test)] arrtibute;
+    // test functions are marked with the #[test] attribute
+
+    // 2. doc testing
+
+    // 3. integration testing
+
+    // 4. dev-dependencies
+}
