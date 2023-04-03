e01a74965f55de150829d6c9489b0df2712bc749
gh-liu <liugh.cs@gmail.com>
2023-04-03 20:37:47 +0800

    feat: unsafe operation

---
+++ unsafeOp/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Unsafe annotations in Rust are used to bypass protections put in place by the compiler
+    // 1. dereferencing raw pointers
+    // 2. calling functions or methods which are unsafe
+    // 3. accessing or modifying static mutable variables
+    // 4. implementing unsafe traits
+
+    // inline assembly
+    // via the asm! macro
+}
